b3b202cc1f53c4a58728d70f1568d5faba378943
jiangwei <wwpptogether@gmail.com>
2022-06-04 13:50:36 +0800

    build new version

--- init.sh
+++ init.sh
@@ -6,4 +6,14 @@ log () {
 
 sudo chown -R novice:novice $HOME/.ssh/
 
+# set ip gw show run in --privileged mode, 
+# if not but supply the vars, it will naturally failed 
+if [[ -n "$ip" && -n "$mask" ]]; then
+    sudo ifconfig eth0 "$ip" netmask "$mask"
+fi
+
+if [[ -n "$gw" ]]; then
+    sudo route add default gw "$gw"
+fi
+
 sudo /usr/sbin/sshd -D
