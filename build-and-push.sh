160d17ee0d1b99fb5e4602d92964119279d320c8
jiangwei <wwpptogether@gmail.com>
2022-06-04 16:40:20 +0800

    fix typo

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,7 +4,7 @@ log () {
     printf "[%(%Y-%m-%d %T)T] %s\n" -1 "$*"
 }
 git add .
-git commit -am"${1="build new version"}"
+git commit -am"${1:-"build new version"}"
 git push origin 20.04
 
 docker pull ubuntu:20.04
